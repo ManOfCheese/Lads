dca68e220a68d2229587c0cb3283dbdcc5c1b833
CupcakeMyDucktape <36536272+CupcakeMyDucktape@users.noreply.github.com>
2020-01-30 17:47:21 +0100

    Level playable, with win condition only needs enemy.

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
+
     {
-        if (other.gameObject.tag == "Player"){
-            //other.gameObject.GetComponent<Player>().
+
+  if (other.gameObject.tag == "Player"){
+            other.gameObject.GetComponent<Player>().Win();
+
+
+
+
         }
+
+
+
+
+
     }
-    // Start is called before the first frame update
+    // Start is called before the first frame update. I use this to make Sam Suffer
     void Start()
+
     {
-        
+
+
+
+
+
     }
 
+
     // Update is called once per frame
+
     void Update()
+
     {
-        
+
+        //Nee.  
+
+
+
     }
-}
+
+
+                      }
