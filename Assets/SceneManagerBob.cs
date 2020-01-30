78284aa262646a9230968adddc8b44bf26721021
eMsylf <bobjeltes@hotmail.com>
2020-01-30 15:25:49 +0100

    [+] Menu Scene buttons and functionality

---
+++ Assets/SceneManagerBob.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerBob : MonoBehaviour
+{
+    public void NextScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCount)
+        {
+            Debug.LogWarning("Next scene couldn't be loaded. Current scene build index is equal to scene count.");
+            return;
+        }
+        Debug.Log("Load next scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void CycleScenes()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCount)
+        {
+            Debug.Log("Next scene couldn't be loaded. Current scene build index is equal to scene count. Loading first scene");
+            SceneManager.LoadScene(0);
+            return;
+        }
+        NextScene();
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit application");
+        Application.Quit();
+    }
+}
