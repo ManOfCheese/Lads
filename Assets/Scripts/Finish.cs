80a4c12529d0527b64e668d731c0bf987adc9410
CupcakeMyDucktape <36536272+CupcakeMyDucktape@users.noreply.github.com>
2020-01-30 17:39:37 +0100

    EwE

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player"){
+            //other.gameObject.GetComponent<Player>().
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
